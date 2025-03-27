97c817da971f428346ebc5cd1636442378b8920f
zakariaiiii <zakarialhmidiidrissi2@gmail.com>
2025-03-27 22:51:46 +0000

    new look

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function showMessage() {
     alert("مرحبا! هذا أول موقع لك 🎉");
 }
+document.addEventListener("DOMContentLoaded", function () {
+    let votes = {
+        "BIG A": 0,
+        "mirv48": 0,
+        "Shaoline": 0
+    };
+
+    const voteBtn = document.getElementById("vote-btn");
+
+    voteBtn.addEventListener("click", function () {
+        const selectedArtist = document.querySelector('input[name="artist"]:checked');
+        
+        if (selectedArtist) {
+            votes[selectedArtist.value]++;
+            updateResults();
+            alert("شكراً لتصويتك!");
+        } else {
+            alert("يرجى اختيار فنان للتصويت.");
+        }
+    });
+
+    function updateResults() {
+        document.getElementById("bigA-votes").innerText = votes["BIG A"];
+        document.getElementById("mirv48-votes").innerText = votes["mirv48"];
+        document.getElementById("shaoline-votes").innerText = votes["Shaoline"];
+    }
+});
